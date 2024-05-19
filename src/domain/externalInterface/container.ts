29dd92e05a2fb51b1df9cb3c0ecc60243d54b583
statiolake <statiolake@gmail.com>
2024-05-20 01:01:55 +0900

    fix: add channel to ServiceContainer

--- src/domain/externalInterface/container.ts
+++ src/domain/externalInterface/container.ts
@@ -1,10 +1,11 @@
-import { Mutex } from 'coc.nvim';
+import { Mutex, OutputChannel } from 'coc.nvim';
 import { InstallExtensionsInteractor } from '../usecaseInterface/installExtensionsUsecase';
 import { UninstallUnusedExtensionsInteractor } from '../usecaseInterface/uninstallUnusedExtensionsUsecase';
 import { ConfigLoaderInterface } from './configLoaderInterface';
 import { UserPromptInterface } from './userPromptInterface';
 
 export type ServiceContainer = {
+  channel: OutputChannel;
   installerMutex: Mutex;
 
   configLoader: ConfigLoaderInterface;
