bca37c7cc80e9635d73a988a4b93e4a276d60e5a
statiolake <statiolake@gmail.com>
2024-05-20 00:47:41 +0900

    feat: do not ask for once cancelled languages again

--- src/controller/events.ts
+++ src/controller/events.ts
@@ -1,11 +1,22 @@
 import { LinesTextDocument } from 'coc.nvim';
 import { createContainer } from '../containerFactory';
 
+const cancelledLanguages = {};
+
 export const onDidOpenTextDocument = async (e: LinesTextDocument) => {
+  if (cancelledLanguages[e.languageId]) {
+    // Never ask for the once cancelled language again
+    return;
+  }
+
   const container = createContainer({ silent: true });
   const config = container.configLoader.load();
-  container.installExtensionUsecase.handle({
+  const res = await container.installExtensionUsecase.handle({
     autoExecution: config.autoLanguageInstall,
     language: e.languageId,
   });
+
+  if (res.detail === 'cancelled') {
+    cancelledLanguages[e.languageId] = true;
+  }
 };
